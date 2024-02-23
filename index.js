a4c839135078a2006271c21c6d47f94969cffdf7
fatima <fatma.anime.taha@gmail.com>
2024-02-23 22:44:10 +0300

    2push

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 function processAges(ages) {
-
   doubledAges=[]
    for (let i of ages) {
-    
     doubledAges.push(i*2)
   } 
   console.log(doubledAges);
     ages.forEach(age => {
     console.log(age);
-    
   });
+
    let adults =ages.filter(age =>{
      return age>=18 
-    
-    
   })
    console.log(adults);
 
    let ageStrings =ages.map (age=>{
     return age.toString()
-  }
-    )
+  })
   console.log(ageStrings);
 
   return { doubledAges, adults, ageStrings };
